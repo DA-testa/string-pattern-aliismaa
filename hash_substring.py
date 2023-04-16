e224ae3f85cc35b51782eb34ef870d714e7c530d
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-04-16 17:00:20 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,46 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+  
+    choice = input()
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    if "I" in choice:
+        pattern = input().strip()
+        txt = input().strip()
+        return pattern, txt
+        
+    elif "F" in choice:
+        filename = "06"
+        if "a" in filename:
+            print("File names with letter a are not allowed")
+            return
+        try:
+            with open('tests/' + filename, 'r') as file:
+                pattern = f.readline().strip()
+                txt = f.readline().strip()
+                
+        except FileNotFoundError:
+            print("Error")
+            return
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    occurrences = []
+    find_len = len(pattern)
+    txt_len = len(txt)
+    find_hesh = hash(pattern)
+    txt_hesh = hash(txt[:find_len])
+    for i in range(len(txt) - len(pattern) + 1):
+        if pattern_hash == txt_hash:
+            if pattern == txt[i:i+len(pattern)]:
+                occurrences.append(i)
+        if i < len(txt) - len(pattern):
+            txt_hash = txt_hash - ord(txt[i]) + ord(txt[i+len(pattern)])
+            txt_hash %= 101
+    return occurrences
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
